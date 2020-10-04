8977dfff988c3caaf0532d64c14096920d9e2f81
Albert S <albertsigmaeng@gmail.com>
2020-10-04 17:59:24 +0200

    added a test class that gets and inputs a number

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("TEST");
+        TestClass tc = new TestClass();
+        Scanner in = new Scanner(System.in);
+
+        int i = 0;
+
+        System.out.print("Intro a test number: ");
+        i = in.nextInt();
+
+        tc.setTestInteger(i);
+
+        System.out.println("The number you entered is: " + tc.getTestInteger());
     }
 }
---
+++ src/com/company/TestClass.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class TestClass {
+    private int testInteger;
+
+    public void setTestInteger(int i){
+        this.testInteger = i;
+    }
+
+    public int getTestInteger(){
+        return this.testInteger;
+    }
+}
